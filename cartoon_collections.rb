b44ca6f6d5a847c0115a12a04698a36da41d0693
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 21:55:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+ arr.any? { |elem| elem.length > 4}
 end
 #
 # def find_the_cheese# code an argument here
