b6d2fca473bca66d68dd416ff492cbfa7f8699b9
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 21:50:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,14 +6,15 @@ def roll_call_dwarves(dwarves)# code an argument here
  end
 end
 
-def summon_captain_planet(arr)
-  arr.map do |elem|
+def summon_captain_planet(array)
+  array.map do |elem|
     elem.capitalize << "!"
   end
 end
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
+
+def long_planeteer_calls(array)
+  # Your code here
+end
 #
 # def find_the_cheese# code an argument here
 #   # the array below is here to help
